26f56aa6ebd9c0ba542f86eb70fc13e83444921a
Henrique Cestaro <hjcestaro@hotmail.com>
2021-09-13 15:32:08 -0300

    inserção de js e html

---
+++ app.js
@@ -0,0 +1,10 @@
+const icon = document.getElementById("icon");
+
+icon.onclick = () => {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "img//sun.png";
+  } else {
+    icon.src = "img//moon.png";
+  }
+};
